becfd984a6bb0a5ffeadb253ecc3567a477c2cc8
amberjblue <amber.blue.n@gmail.com>
2022-03-05 18:13:08 -0500

    Add everything

---
+++ src/components/LinkList.js
@@ -0,0 +1,163 @@
+import React from "react";
+import Link from './Link'
+import { useQuery, gql } from "@apollo/client";
+import { useLocation, useNavigate } from 'react-router-dom';
+import { LINKS_PER_PAGE } from '../constants';
+
+
+const NEW_VOTES_SUBSCRIPTION = gql`
+  subscription {
+    newVote {
+      id
+      link {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`;
+
+export const FEED_QUERY = gql`
+  query FeedQuery(
+    $take: Int
+    $skip: Int
+    $orderBy: LinkOrderByInput
+  ) {
+    feed(take: $take, skip: $skip, orderBy: $orderBy) {
+      id
+      links {
+        id
+        createdAt
+        url
+        description
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      count
+    }
+  }
+`;
+
+const LinkList = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const isNewPage = location.pathname.includes(
+    'new'
+  );
+
+  const pageIndexParams = location.pathname.split(
+    '/'
+  );
+
+  const page = parseInt(
+    pageIndexParams[pageIndexParams.length - 1]
+  );
+
+  const pageIndex = page ? (page - 1) * LINKS_PER_PAGE : 0;
+
+  const getQueryVariables = (isNewPage, page) => {
+    const skip = isNewPage ? (page - 1) * LINKS_PER_PAGE : 0;
+    const take = isNewPage ? LINKS_PER_PAGE : 100;
+    const orderBy = { createdAt: 'desc' };
+    return { take, skip, orderBy };
+  };
+
+   // Calculate list of links to be rendered
+   const getLinksToRender = (isNewPage, data) => {
+    if (isNewPage) {
+      return data.feed.links;
+    }
+    const rankedLinks = data.feed.links.slice();
+    rankedLinks.sort(
+      (l1, l2) => l2.votes.length - l1.votes.length
+    );
+    return rankedLinks;
+  };
+  
+  const {
+    data,
+    loading,
+    error,
+    subscribeToMore
+  } = useQuery(FEED_QUERY, {
+    variables: getQueryVariables(isNewPage, page),
+    fetchPolicy: "cache-and-network"  // see note for explanation
+  });
+
+  subscribeToMore({
+    document: NEW_VOTES_SUBSCRIPTION
+  });
+
+  return (
+    <>
+      {loading && <p>Loading...</p>}
+      {error && <pre>{JSON.stringify(error, null, 2)}</pre>}
+      {data && (
+        <>
+          {getLinksToRender(isNewPage, data).map(
+            (link, index) => (
+              <Link
+                key={link.id}
+                link={link}
+                index={index + pageIndex}
+              />
+            )
+          )}
+          {isNewPage && (
+            <div className="flex ml4 mv3 gray">
+              <div
+                className="pointer mr2"
+                onClick={() => {
+                  if (page > 1) {
+                    navigate(`/new/${page - 1}`);
+                  }
+                }}
+              >
+                Previous
+              </div>
+              <div
+                className="pointer"
+                onClick={() => {
+                  if (
+                    page <=
+                    data.feed.count / LINKS_PER_PAGE
+                  ) {
+                    const nextPage = page + 1;
+                    navigate(`/new/${nextPage}`);
+                  }
+                }}
+              >
+                Next
+              </div>
+            </div>
+          )}
+        </>
+      )}
+    </>
+  );
+};
+
+export default LinkList;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,72 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './styles/index.css';
+import App from './components/App';
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter } from 'react-router-dom';
+import { AUTH_TOKEN } from './constants';
+import { split } from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { getMainDefinition } from '@apollo/client/utilities';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// Import all the dependencies we need to wire up the Apollo client
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client';
+
+// Connect our ApolloClient instance with the GraphQL API
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  };
+});
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000/graphql`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN)
+    }
+  }
+});
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return (
+      kind === 'OperationDefinition' &&
+      operation === 'subscription'
+    );
+  },
+  wsLink,
+  authLink.concat(httpLink)
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// We instantiate ApolloClient by passing in the httpLink and a new instance of an InMemoryCache.
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache()
+});
+// Render the root component of our React app. The App is wrapped with the higher-order component ApolloProvider that gets passed the client as a prop.
+ReactDOM.render(
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
+ 
